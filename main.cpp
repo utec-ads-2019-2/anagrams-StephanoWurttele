99f12fce7fc5108f49f9625b1ff38f428f4b8025
Stephano <stephano.wurttele@utec.edu.pe>
2019-08-25 23:49:03 -0500

    https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=395

--- main.cpp
+++ main.cpp
@@ -6,47 +6,47 @@
 #include <vector>
 using namespace std;
 int main() {
-	int n;
-	string temp;
-	map<char,int> temp2;
-	vector<map<char,int>> strings={};
-	vector<string> strings2={};
-	vector<string> answers;
-	cin>>n;
-	for (int i=-2;i<n;i++){
-		while(getline(cin,temp)){
-			if(temp==""){
-				break;
-			}
-			for (int o=0;o<temp.length();o++){
-				if(temp[o]!=' ')
-					temp2[temp[o]]++;
-			}
-			strings2.push_back(temp);
-			strings.push_back(temp2);
-			temp2.clear();
-		}
-	
-		
-		for (int j=0;j<strings.size();j++){
-			for (int u=j+1;u<strings.size();u++){
-				if(strings[j]==strings[u]){
-					if(strings2[j].compare(strings2[u])<0)
-						answers.push_back(strings2[j]+" = "+strings2[u]+"\n");
-					else
-						answers.push_back(strings2[u]+" = "+strings2[j]+"\n");	
-				}
-			}
-		}
-		answers.push_back("o\n");
-		sort(answers.begin(),answers.end());
-		for (auto it=answers.begin();it!=answers.end();it++)
-			cout<<*it;
-	strings.clear();
-	strings2.clear();
-	answers.clear();
-	}
-	
-	return 0;
-}
+        int n;
+        string temp;
+        map<char,int> temp2;
+        vector<map<char,int>> strings={};
+        vector<string> strings2={};
+        vector<string> answers;
+        cin>>n;
+        for (int i=-2;i<n;i++){
+                while(getline(cin,temp)){
+                        if(temp==""){
+                                break;
+                        }
+                        for (int o=0;o<temp.length();o++){
+                                if(temp[o]!=' ')
+                                        temp2[temp[o]]++;
+                        }
+                        strings2.push_back(temp);
+                        strings.push_back(temp2);
+                        temp2.clear();
+                }
 
+
+                for (int j=0;j<strings.size();j++){
+                        for (int u=j+1;u<strings.size();u++){
+                                if(strings[j]==strings[u]){
+                                        if(strings2[j].compare(strings2[u])<0)
+                                                answers.push_back(strings2[j]+" = "+strings2[u]+"\n");
+                                        else
+                                                answers.push_back(strings2[u]+" = "+strings2[j]+"\n");
+                                }
+                        }
+                }
+                if(i>0)
+                    answers.push_back("\n");
+                sort(answers.begin(),answers.end());
+                for (auto it=answers.begin();it!=answers.end();it++)
+                        cout<<*it;
+        strings.clear();
+        strings2.clear();
+        answers.clear();
+        }
+
+        return 0;
+}
