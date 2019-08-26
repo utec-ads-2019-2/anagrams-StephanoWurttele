6ee2c0bf9bccaa45d4595b1cba46d00099c1a86c
Stephano <stephano.wurttele@utec.edu.pe>
2019-08-25 23:57:48 -0500

    https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=395

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main() {
                         }
                 }
                 if(i>0)
-                    answers.push_back("\n");
+                        answers.push_back("\n");
                 sort(answers.begin(),answers.end());
                 for (auto it=answers.begin();it!=answers.end();it++)
                         cout<<*it;
